d6a931697377a25f980d3fa38c82ce72824a7f45
Colin Kelly <colinkelly3@gmail.com>
2026-02-08 23:27:36 -0600

    Adding Map with collisions, and basic player

---
+++ Entities/Player/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+
+func _physics_process(delta):
+	var dir = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	velocity = dir.normalized() * 100
+	move_and_slide()
